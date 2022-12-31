79c4adb0e660af546b2f4d229095e1a54223ce16
Turzum <103773518+Turzum@users.noreply.github.com>
2022-12-31 11:17:09 +0000

    Add files via upload

---
+++ dirty-pipe-check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# usage
+# Check current kernel version ./dpipe.sh
+# Check specific kernel version ./dpipe.sh 5.10.102
+kernel=$1
+major=$(echo ${kernel:-$(uname -r | cut -d '-' -f1)} | cut -d '.' -f1)
+minor=$(echo ${kernel:-$(uname -r | cut -d '-' -f1)} | cut -d '.' -f2)
+patch=$(echo ${kernel:-$(uname -r | cut -d '-' -f1)} | cut -d '.' -f3)
+echo $major $minor $patch
+if (( ${major:-0} < 5 )) ||
+   (( ${major:-0} == 5 && ${minor:-0} < 8 )) ||
+   (( ${major:-0} == 5 && ${minor:-0} == 10 && ${patch:-0} == 102 )) ||
+   (( ${major:-0} == 5 && ${minor:-0} == 10 && ${patch:-0} == 92 )) ||
+   (( ${major:-0} == 5 && ${minor:-0} == 15 && ${patch:-0} == 25 )) ||
+   (( ${major:-0} == 5 && ${minor:-0} >= 16 && ${patch:-0} >= 11 )) ||
+   (( ${major:-0} == 5 && ${minor:-0} > 16 ));
+then
+    echo Not vulnerable
+    exit 0
+else
+    echo Vulnerable
+    exit 1
+fi
